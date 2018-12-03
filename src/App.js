01d898e64af4a5e4084998d09793947fd01494eb
James 'Mac' Read <james.macgregor.read@gmail.com>
2018-12-03 14:05:25 -0700

    completed excercise

--- src/App.js
+++ src/App.js
@@ -1,28 +1,175 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import EnhancedTableHead from './EnhancedTableHead';
+import axios from 'axios';
 
-class App extends Component {
+
+function desc(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function stableSort(array, cmp) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = cmp(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map(el => el[0]);
+}
+
+function getSorting(order, orderBy) {
+  return order === 'desc' ? (a, b) => desc(a, b, orderBy) : (a, b) => -desc(a, b, orderBy);
+}
+
+async function f() {
+  let promise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("I waited 10 seconds before displaying using Async/Await"), 10000)
+  });
+
+  let result = await promise; // wait till the promise resolves (*)
+
+  alert(result); // "done!"
+}
+
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+  },
+  table: {
+    minWidth: 1020,
+  },
+  tableWrapper: {
+    overflowX: 'auto',
+  },
+});
+
+class App extends React.Component {
+  constructor() {
+    super()
+    
+    this.state = {
+      order: 'asc',
+      orderBy: 'episode',
+      data: [],
+      page: 0,
+      rowsPerPage: 9,
+    };
+  }
+
+  componentDidMount(){
+    axios.get(`https://swapi.co/api/films`).then( results => {
+      const movieData = results.data.results.map((movie, idx) => {
+        return { id: idx, episode: movie.episode_id, title: movie.title, director: movie.director, producer: movie.producer, release: movie.release_date }
+      })
+      this.setState({ data: movieData })
+    });
+
+    let sortOrder = localStorage.getItem('sortOrder');
+    if (sortOrder) {
+      this.setState({ order: sortOrder })
+    }else{
+      this.setState({ order: 'asc' })
+    }
+
+    let sortOrderBy = localStorage.getItem('sortOrderBy');
+    if (sortOrderBy) {
+      this.setState({ orderBy: sortOrderBy })
+    }else{
+      this.setState({ orderBy: 'episode' })
+    }
+
+
+    f();
+
+  }
+
+
+  handleRequestSort = (event, property) => {
+    const orderBy = property;
+    let order = 'desc';
+
+    if (this.state.orderBy === property && this.state.order === 'desc') {
+      order = 'asc';
+    }
+
+    this.setState({ order, orderBy });
+
+    localStorage.setItem('sortOrder', order)
+    localStorage.setItem('sortOrderBy', orderBy)
+
+  };
+
+  handleChangePage = (event, page) => {
+    this.setState({ page });
+  };
+
+  handleChangeRowsPerPage = event => {
+    this.setState({ rowsPerPage: event.target.value });
+  };
+
   render() {
+    const { classes } = this.props;
+    const { data, order, orderBy, rowsPerPage, page } = this.state;
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Paper className={classes.root}>
+        <Typography>
+          Star Wars Films
+        </Typography>
+        <div className={classes.tableWrapper}>
+          <Table className={classes.table} aria-labelledby="tableTitle">
+            <EnhancedTableHead
+              order={order}
+              orderBy={orderBy}
+              onRequestSort={this.handleRequestSort}
+              rowCount={data.length}
+            />
+            <TableBody>
+              {stableSort(data, getSorting(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map(n => {
+                  return (
+                    <TableRow hover key={n.id}>
+                      <TableCell numeric>{n.episode}</TableCell>
+                      <TableCell>{n.title}</TableCell>
+                      <TableCell>{n.director}</TableCell>
+                      <TableCell>{n.producer}</TableCell>
+                      <TableCell numeric>{n.release}</TableCell>
+                    </TableRow>
+                  );
+                })}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: 49 * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+      </Paper>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
---
+++ src/EnhancedTableHead.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Tooltip from '@material-ui/core/Tooltip';
+
+const rows = [
+  { id: 'episode', numeric: true, disablePadding: false, label: 'Episode' },
+  { id: 'title', numeric: false, disablePadding: false, label: 'Title' },
+  { id: 'director', numeric: false, disablePadding: false, label: 'Director' },
+  { id: 'producer', numeric: false, disablePadding: false, label: 'Producer' },
+  { id: 'release', numeric: true, disablePadding: false, label: 'Release Date' },
+];
+
+class EnhancedTableHead extends React.Component {
+  createSortHandler = property => event => {
+    this.props.onRequestSort(event, property);
+  };
+
+  render() {
+    const { order, orderBy } = this.props;
+
+    return (
+      <TableHead>
+        <TableRow>
+          {rows.map(row => {
+            return (
+              <TableCell
+                key={row.id}
+                numeric={row.numeric}
+                padding={row.disablePadding ? 'none' : 'default'}
+                sortDirection={orderBy === row.id ? order : false}
+              >
+                <Tooltip
+                  title="Sort"
+                  placement={row.numeric ? 'bottom-end' : 'bottom-start'}
+                  enterDelay={300}
+                >
+                  <TableSortLabel
+                    active={orderBy === row.id}
+                    direction={order}
+                    onClick={this.createSortHandler(row.id)}
+                  >
+                    {row.label}
+                  </TableSortLabel>
+                </Tooltip>
+              </TableCell>
+            );
+          }, this)}
+        </TableRow>
+      </TableHead>
+    );
+  }
+}
+
+EnhancedTableHead.propTypes = {
+  onRequestSort: PropTypes.func.isRequired,
+  order: PropTypes.string.isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired,
+};
+
+
+export default (EnhancedTableHead);
